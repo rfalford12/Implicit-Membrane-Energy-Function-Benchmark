fc1fc2b4f369121998ac1e52ba4899e2ec40013a
Rebecca Alford <rfalford12@gmail.com>
2019-04-15 13:05:30 -0400

    Adding data generation scripts

---
+++ src/python/hpc_util.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python
+###################################################################
+#@file:         hpc_util.py                                                                                  
+#@description:  Methods for submitting Rosetta jobs to SLURM and CONDOR clusters                                             
+#@args:			jobname, executable, arguments, queue_no, high_mem                                                
+#@author: 		Rebecca F. Alford                   
+#@email: 		rfalford12@gmail.com                                          
+###################################################################
+
+def make_jobfile( casedir, case, executable, arguments ): 
+
+    jobfile = casedir + "/" + case + "_energy_landscape.sh"
+    with open( jobfile, 'a' ) as f: 
+        f.write( "#!/bin/bash\n" )
+        f.write( executable + " " + arguments + "\n" )
+        f.close()
+    os.system( "chmod +x " + jobfile )
+    return jobfile
+
+
+def submit_condor_job( path, jobname, executable, arguments, queue_no=1, high_mem=False ):
+
+    # Given a test path and
+    filename = path + "/" + jobname + ".condor"
+    with open( filename, 'w' ) as f:
+        f.write( "#!/bin/bash\n" )
+        f.write( "# Automatically generated condor submission file for membrane benchmark\n" )
+        f.write( "Universe = vanilla\n" )
+        f.write( "output = " + path + "/" + jobname + ".out\n" )
+        f.write( "error = " + path + "/" + jobname + ".err\n" )
+        if ( high_mem == True ): 
+            f.write( "request_memory = 15000\n")
+        f.write( "notify_user = rfalford12@gmail.com\n" )
+        f.write( "Executable = " + executable + "\n" )
+        f.write( "Arguments = " + arguments + "\n" )
+        f.write( "Queue " + str(queue_no) + "\n" )
+
+    # Run the condor file
+    os.system( "condor_submit " + filename )
+
+def submit_stampede_job( path, jobname, jobfile, num_nodes=1 ): 
+  
+    # Create a new sbatch file named for the job type and test
+    filename = path + "/" + jobname + ".sbatch" 
+    with open( filename, 'w' ) as f: 
+
+        # Write bash and comments
+        f.write( "#!/bin/bash -l\n" )
+        f.write ( "\n" )
+        f.write( "#----------------------------------------------------\n" )
+        f.write( "# SLURM job script for membrane force field benchmarking applications\n" )
+        f.write( "# Runs on Stampede2 with MPI applications\n" )
+        f.write( "#----------------------------------------------------\n" )
+        f.write( "\n" )
+
+        # Write the job information
+        f.write( "#SBATCH -J " + jobname + "\n" )
+        f.write( "#SBATCH -p normal\n" )
+        f.write( "#SBATCH -N " + str(num_nodes) + "\n" )
+        f.write( "#SBATCH -n 16\n" )
+        f.write( "#SBATCH -t 24:0:0\n" )
+
+        # Write job specific output and reporting information
+        f.write( "#SBATCH -o " + path + "/" + jobname + ".%j.out\n" )
+        f.write( "#SBATCH -e " + path + "/" + jobname + ".%j.err\n" )
+        f.write( "#SBATCH --mail-user=rfalford12@gmail.com\n" )
+        f.write( "#SBATCH --mail-type=ALL\n" )
+        f.write( "#SBATCH -A TG-MCB180056\n" )
+        f.write( "\n" )
+
+        # Specify required modules
+        f.write( "module load intel/18.0.0\n" )
+        f.write( "export MKL_MIC_ENABLE=1\n" )
+
+        # Provide a description of the job
+        f.write(  "echo Starting MPI job running " + jobfile + "\n" )
+
+        # Run the job
+        f.write( "time\n" )
+        f.write( "mpiexec bash " + jobfile + "\n" )
+        f.write( "time\n" )
+
+        f.close()
+
+    # Run the slurm job file
+    sbatch_command = "sbatch " + filename
+    os.system( sbatch_command )
+
+
+def submit_marcc_job( path, jobname, jobfile, num_nodes=1 ): 
+
+    # Create a new sbatch file named for the job type and test
+    filename = path + "/" + jobname + ".sbatch" 
+    with open( filename, 'w' ) as f: 
+
+        # Write bash and comments
+        f.write( "#!/bin/bash -l\n" )
+        f.write ( "\n" )
+        f.write( "#----------------------------------------------------\n" )
+        f.write( "# SLURM job script for membrane force field benchmarking applications\n" )
+        f.write( "# Runs on MARCC with MPI applications\n" )
+        f.write( "#----------------------------------------------------\n" )
+        f.write( "\n" )
+
+        # Write the job information
+        f.write( "#SBATCH --job-name=" + jobname + "\n" )
+        f.write( "#SBATCH --partition=parallel\n" )
+        f.write( "#SBATCH --nodes=" + str(num_nodes) + "\n" )
+        f.write( "#SBATCH --time=60:0:0\n" )
+        f.write( "#SBATCH --mem=120GB\n" )
+
+        # Write job specific output and reporting information
+        f.write( "#SBATCH --output " + path + "/" + jobname + ".%j.out\n" )
+        f.write( "#SBATCH --error " + path + "/" + jobname + ".%j.err\n" )
+        f.write( "#SBATCH --mail-user=rfalford12@gmail.com\n" )
+        f.write( "#SBATCH --mail-type=ALL\n" )
+        f.write( "\n" )
+
+        # Soecify required modules
+        f.write( "module unload openmpi gcc\n" )
+        f.write( "module load intel-mpi git\n" )
+        f.write( "ml --gcc\n")
+        f.write( "ml gcc/4.8.2\n")
+
+        # Provide a description of the job
+        f.write(  "echo Starting MPI job running " + jobfile + "\n" )
+
+        # Run the job
+        f.write( "time\n" )
+        f.write( "mpirun ./" + jobfile + "\n" )
+        f.write( "time\n" )
+
+        f.close()
+
+    # Run the slurm job file
+    sbatch_command = "sbatch " + filename
+    os.system( sbatch_command )
---
+++ src/python/make_peptide_energy_landscape.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+###################################################################
+#@file:         make_peptide_energy_landscape.py                                                                                   
+#@description:  Generate orientation-dependent peptide energy map                                               
+#@args:			--energy_fxn (wts) --targets (list of targets)                                                          
+#@author: 		Rebecca F. Alford                   
+#@email: 		rfalford12@gmail.com                                          
+###################################################################
+
+import random
+import hpc_util, read_config
+from string import Template
+from optparse import OptionParser, IndentedHelpFormatter
+_script_path_ = os.path.dirname( os.path.realpath(__file__) )
+
+def run_peptide_energy_landscape_calc( energy_fxn, config, targets, test_name, xml, pH_mode=False, pH_value=7 ): 
+	"""
+	A function to generate orientation-dependent peptide energy landscapes
+
+	Arguments: 
+		energy_fxn = energy function to use for calculations (typically, name of the weights file)
+		config = path to benchmark, Rosetta executables
+		targets = list of targets
+		xml = Path to RosettaScript defining the peptide landscape search protocol
+		pH_Mode = Use pH energy in landscape calculations
+		pH_value = Physiological pH is 7
+	"""
+
+    print( "Making energy landscapes for the helical peptide set", targets ) 
+
+   	# Read list of energy landscape test cases
+    list_of_targets = config.benchmark_path + "targets/" + targets + "/targets.list"
+    with open( list_of_targets, 'rt' ) as f: 
+        test_cases = f.readlines()
+    test_cases = [ x.strip() for x in test_cases ]
+
+    # Generate path to executable
+    executable = config.rosetta_path + "rosetta_scripts" + "." + config.platform + config.compiler + config.buildenv
+    xml_script =  config.benchmark_path + "/src/xml/" + xml_protocol
+
+    # Change directories to a data analysis dir
+    outdir = config.benchmark_path + "/data/" + energy_fxn + "/" + test_name 
+    if ( not os.path.isdir( outdir ) ): 
+    	os.system( "mkdir " + outdir )
+    	os.chdir( outdir )
+
+    # For each test case, generate specific arguments, a condor file, and then run
+    for case in test_cases:
+
+        # Setup case-specific variables (pdbfile, spanfile, xmlargs)
+        pdbfile = targets + "/" + case + "/" + case + ".pdb"
+        spanfile = targets + "/" + case + "/" + case + ".span"
+
+        # Default composition is DLPC
+        # Should I tune the pH of my simulation?
+        arguments = " -overwrite -in:file:s " +  pdbfile + " -mp:setup:spanfiles " + spanfile + " -parser:script_vars sfxn_weights=" + energy_fxn + " -parser:protocol " + xml_script 
+        arguments = arguments + " -mp:lipids:composition DLPC "
+        if ( pH_Mode == True ): 
+        	arguments = arguments + " -pH_mode true -value_pH " + pH
+  		
+  		casedir = outdir + "/" + case
+ 		if ( pH_mode == True ): 
+ 			casedir = casedir + "_pH" + str(pH_value)
+
+  		if ( not os.path.isdir( casedir ) ): 
+  			os.system( "mkdir " + casedir )
+  			os.chdir( casedir )
+
+  		# Write jobfile and submit to the HPC
+  		print("Submitting orientation sampling calculations for energy landscape case:", case ) 
+  		jobname = case + "_energy_landscape"
+		jobfile = hpc_util.make_jobfile( casedir, case, executable, arguments )
+		hpc_util.submit_condor_job( config.benchmark_path, jobname, jobfile, "" )
