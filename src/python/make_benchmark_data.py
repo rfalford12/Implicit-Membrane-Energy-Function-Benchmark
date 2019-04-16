e01c3f0f528147840983789055e56e51d0e47190
Rebecca Alford <rfalford12@gmail.com>
2019-04-16 10:00:40 -0400

    Fixing namespacing in calls to make functions

--- src/python/make_benchmark_data.py
+++ src/python/make_benchmark_data.py
@@ -80,16 +80,16 @@ def main( args ):
 
 		# Make native refined structures
 		nstruct_native_refined = 20
-		run_refinement_calc( Options.energy_fxn, config, "structure/D2_singe_pass_mp_complexes", "protein-protein-docking/", "mp_relax.xml", nstruct_native_refined )
-		run_refinement_calc( Options.energy_fxn, config, "structure/D3_multi_pass_mp_complexes/large-bound-set", "protein-protein-docking", "mp_relax.xml", nstruct_native_refined )
+		make_refined_decoys.run_refinement_calc( Options.energy_fxn, config, "structure/D2_singe_pass_mp_complexes", "protein-protein-docking/", "mp_relax.xml", nstruct_native_refined )
+		make_refined_decoys.run_refinement_calc( Options.energy_fxn, config, "structure/D3_multi_pass_mp_complexes/large-bound-set", "protein-protein-docking", "mp_relax.xml", nstruct_native_refined )
 		#run_refinement_calc( Options.energy_fxn, config, "structure/D3_multi_pass_mp_complexes/large-unbound-set", "protein-protein-docking", "mp_relax.xml", nstruct_native_refined )
 
 		# Make refined homology models 
 		#run_refinement_calc( Options.energy_fxn, )
 
 		# Make protein-protein complex models via rigid body docking
-		run_docking_calc( Options.energy_fxn, config, "structure/D2_singe_pass_mp_complexes", "protein-protein-docking" )
-		run_docking_calc( Options.energy_fxn, config, "structure/D3_multi_pass_mp_complexes", "protein-protein-docking" )
+		make_asymm_docked_complexes.run_docking_calc( Options.energy_fxn, config, "structure/D2_singe_pass_mp_complexes", "protein-protein-docking" )
+		make_asymm_docked_complexes.run_docking_calc( Options.energy_fxn, config, "structure/D3_multi_pass_mp_complexes", "protein-protein-docking" )
 
 
 if __name__ == "__main__" : main(sys.argv)
