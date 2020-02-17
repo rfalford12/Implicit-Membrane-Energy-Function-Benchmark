736e7805021374c2959e3f00f733a0adf10bd016
rfalford12 <rfalford12@gmail.com>
2020-02-17 16:19:43 -0500

    updating test08 documentation

--- tests/R/test08_sequence_recovery.R
+++ tests/R/test08_sequence_recovery.R
@@ -1,6 +1,12 @@
-# @file: plot_protein_design_results.R
-# @brief: Plot protein desgin results
-# @author: Rebecca F. Alford (ralford3@jhu.edu)
+#' Analyze sequence recovery results (Test #8)
+#' 
+#' Computes analysis for sequence recovery, including: 
+#'    - Recovery rates
+#'    - KL Divergence
+#'    - Amino acid composition
+#'
+#' @author Rebecca Alford <ralford3[at]jhu.edu>
+#' 
 
 library(cowplot)
 library(reshape2)
@@ -8,17 +14,16 @@ library(ggrepel)
 library(viridisLite)
 library(viridis)
 
-
-# Files containing all of the data
-total.seq.recov <- "total_sequence_recovery.dat"
-total.kl.divergence <- "total_sequence_divergence.dat"
-per.aa.seq.recov <- "per_amino_acid_recovery.dat"
-per.aa.kl.divergence <- "per_amino_acid_divergence.dat"
-per.class.seq.recov <- "per_amino_acid_class_recovery.dat"
-per.class.kl.divergence <- "per_amino_acid_class_divergence.dat"
-
-# For each energy function, calculate the fraction of per_AA recov that is above 0.05
-per.aa.rates <- read.table( paste( workdir, per.aa.seq.recov, sep = "/"), header = T)
+#' Compute the fraction of amino acid types with recovery rates above random
+#' or the general background probability of 0.05 (1 in 20)
+#' 
+#' @param df Dataframe with amino acid design and native distribution data
+#'
+#' @return Plot object
+#'
+#' @examples
+#' frac.aa.recov.above.rand (d, "f19f)
+#'
 frac.aa.recov.above.rand <- function( per.aa.rates, efxn ) {
   
   overall.frac <- count_if( gt(0.05), per.aa.rates$overall[which( per.aa.rates$efxn == efxn)] )/20
@@ -36,187 +41,91 @@ frac.aa.recov.above.rand <- function( per.aa.rates, efxn ) {
   return(df)
 }
 
-
-frac.recov.rates <- rbind( m07.recov.rates, m12.recov.rates, m19.recov.rates, r15.recov.rates )
-
-
-load.design.performance.data <- function( recov.fn, div.fn, run.list ) {
-
-  # Read the recovery and divergence data into melted dataframes
-  df.total.recov <- read.table( paste( workdir, recov.fn, sep = "/"), header = T )
-  trcov <- melt(df.total.recov, id.vars = c("efxn"), value.name = "recovery", variable.name = "solvation" )
-  df.total.div <- read.table( paste( workdir, div.fn, sep = "/"), header = T )
-  tdiv <- melt(df.total.div, id.vars = c("efxn"), value.name = "divergence", variable.name = "solvation" )
-
-  # Merge into a single dataframe
-  total.stats <- trcov
-  total.stats$divergence <- tdiv$divergence
-
-  # Use the "tag" to log the energy function version, species/taxonomy, and lipid types
-  efxn.versions <- c()
-  subset <- c()
-  lipid.types <- c()
-  for (efxn.type in total.stats$efxn) {
-    efxn.array <- strsplit(efxn.type, "_")
-    efxn.versions <- c( efxn.versions, efxn.array[[1]][1] )
-    subset <- c( subset, efxn.array[[1]][2] )
-    if ( length(efxn.array[[1]]) > 2 ) {
-      lipid.types <- c( lipid.types, efxn.array[[1]][3])
-    } else {
-      lipid.types <- c( lipid.types, "none")
-    }
-  }
-  total.stats$efxn.version <- efxn.versions
-  total.stats$subset <- subset
-  total.stats$lipid.types <- lipid.types
-
-  # Pull specific energy function versions for this data frame
-  total.stats.subset <- total.stats[ which( is.element( total.stats$efxn, run.list ) ), ]
-  return(total.stats.subset)
-}
-
-plot.overall.design.performance <- function( df ) {
-  design.performance <- ggplot( data = df, aes( x = divergence, y = recovery, fill = lipid.types ) ) +
+#' Make a plot to summarize the results of sequence recovery calculations 
+#'
+#' @param df Dataframe with amino acid design and native distribution data
+#'
+#' @return Plot object
+#'
+#' @examples
+#' make.sequence.recovery.plot(df)
+#'
+make.sequence.recovery.plot <- function(df) {
+  recovery.rates <- ggplot( data = df, aes( x = recovery, y = frac.recov.above.random*20, shape = solvation ) ) +
     background_grid() +
-    geom_point( aes( color = lipid.types ), size = 0.5 ) +
-    geom_label_repel( aes( label = efxn.version ), force = 0.75, size = 2, label.padding = 0.125, label.size = 0.15, segment.size = 0.25 ) +
-    geom_hline( yintercept = 0.05, color = "gray60", linetype = "dashed", size = 0.25 ) +
-    geom_vline( xintercept = 0, color = "gray60", linetype = "dashed", size = 0.25 ) +
-    scale_y_continuous( "Sequence Recovery (%)", limits = c(0, NA) ) +
-    scale_x_continuous( "KL Divergence", expand = c(0.1, 0.1) ) +
-    scale_fill_manual( values = c( "#b3cde3", "#bdbdbd", "#fbb4ae" ) ) +
-    scale_color_manual( values = c( "#b3cde3", "#bdbdbd", "#fbb4ae" ) ) +
-    facet_wrap(  ~ solvation, ncol = 3, nrow = 2, scales = "free_x" ) +
+    geom_point(size = 1.5, color = "gray40") + 
+    geom_text_repel( aes( label = efxn ), size = 2, force = 2 ) +
+    scale_x_continuous( "Recovery (%)", limits = c(0, 0.5), expand = c(0,0) ) +
+    scale_y_continuous( "# AAs with non-random Recovery", limits = c(0, 20), expand = c(0,0) ) +
+    scale_shape_manual( values = c(15, 16, 1, 2, 17) ) + 
+    facet_wrap(  ~ which_panel, ncol = 3, nrow = 1, scales = "free" ) +
+    theme_bw() + 
     theme( legend.position = "none",
            text = element_text( size = 8 ),
            axis.text = element_text( size = 8 ),
            axis.line.x = element_line( size = 0.25 ),
            axis.line.y = element_line( size = 0.25 ),
            axis.ticks = element_line( size = 0.25 ),
-           strip.background = element_blank() )
-  return(design.performance)
+           strip.background = element_blank(), 
+           panel.grid.major = element_line( size = 0.15 ), 
+           panel.grid.minor = element_line( size = 0.15 ), 
+           panel.border = element_rect( size = 0.25 ) ) + 
+    coord_flip()
+  return(recovery.rates)
 }
 
-per.aa.design.perf.density <- ggplot( data = per.aa.stats.all, aes( x = recovery, fill = efxn, color = efxn ) ) +
-  background_grid() +
-  geom_vline( xintercept = 0.05, color = "gray60", linetype = "dashed" ) +
-  geom_density( alpha = 0.1 ) +
-  scale_x_continuous( "Sequence Recovery (%)", limits = c(0, 0.5) ) +
-  scale_y_continuous( "Density", expand = c(0,0) ) +
-  scale_color_brewer(  palette = "Set1", direction = -1 ) +
-  scale_fill_brewer( palette = "Set1", direction = -1 ) +
-  facet_wrap( ~ solvation, scales = "free_y" ) +
-  theme_bw() +
-  theme(  legend.position = "bottom",
-          axis.text = element_text( size = 8 ),
-          text = element_text( size = 8 ) )
+#' Make a long-scale bar graph representing the KL divergence for each 
+#' amino acid type from the native distribution for a given energy function's
+#' design performance
+#'
+#' @param df Dataframe with amino acid design and native distribution data
+#'
+#' @return Plot object
+#'
+#' @examples
+#' plot.aa.divergence(df)
+#'
+plot.aa.divergence <- function( df ) {
+  p <- ggplot( df[ which( overall.df$variable == "overall" & df$efxn != "m19_all_DLPC" & df$efxn != "r15_all_DLPC" ), ], aes( x = residue, y = value, fill = category ) ) + 
+    geom_hline( yintercept = 0, color = "black", size = 0.25 ) + 
+    geom_bar( stat = "identity", color = "black", size = 0.15) + 
+    scale_fill_brewer( palette = "Pastel1" ) + 
+    scale_y_continuous( limits = c(-5, 1), breaks = c(-5, -4, -3, -2, -1, 0, 1), "Divergence" ) + 
+    scale_x_discrete( "" ) + 
+    facet_wrap( ~ efxn, scales = "free" ) + 
+    theme_bw() + 
+    theme( strip.background = element_blank(), 
+           legend.position = "none", 
+           text = element_text( size = 8 ), 
+           axis.text = element_text( size = 7 ), 
+           axis.line.x = element_line( size = 0.25 ), 
+           axis.line.y = element_line( size = 0.25 ), 
+           axis.ticks = element_line( size = 0.25 ), 
+           panel.grid.major = element_line( size = 0.15 ), 
+           panel.grid.minor = element_line( size = 0.15 ), 
+           panel.border = element_rect( size = 0.25 ) )
+  return(p)  
+}
 
-# Current working directory
-workdir <- "/Users/ralford/Dropbox/rebecca-research-shared/writing/papers/BioInspired-Membrane/figures/figure4-design-results-all"
-df <- read.table( paste( workdir, "design_seqrecov_performance.txt", sep = "/"), header = T )
-df$solvation <- factor( df$solvation, c("overall", "buried", "surface", "lipid_facing", "interfacial", "aqueous"))
-df$which_panel <- factor( df$which_panel, c("FALSE", "burial", "memb"))
-recovery.rates <- ggplot( data = df, aes( x = recovery, y = frac.recov.above.random*20, shape = solvation ) ) +
-  background_grid() +
-  geom_point(size = 1.5, color = "gray40") + 
-  geom_text_repel( aes( label = efxn ), size = 2, force = 2 ) +
-  scale_x_continuous( "Recovery (%)", limits = c(0, 0.5), expand = c(0,0) ) +
-  scale_y_continuous( "# AAs with non-random Recovery", limits = c(0, 20), expand = c(0,0) ) +
-  scale_shape_manual( values = c(15, 16, 1, 2, 17) ) + 
-  #scale_fill_manual( values = c( "#b3cde3", "#bdbdbd", "#fbb4ae" ) ) +
-  #scale_color_manual( values = c( "#b3cde3", "#bdbdbd", "#fbb4ae" ) ) +
-  facet_wrap(  ~ which_panel, ncol = 3, nrow = 1, scales = "free" ) +
-  theme_bw() + 
-  theme( legend.position = "none",
-         text = element_text( size = 8 ),
-         axis.text = element_text( size = 8 ),
-         axis.line.x = element_line( size = 0.25 ),
-         axis.line.y = element_line( size = 0.25 ),
-         axis.ticks = element_line( size = 0.25 ),
-         strip.background = element_blank(), 
-         panel.grid.major = element_line( size = 0.15 ), 
-         panel.grid.minor = element_line( size = 0.15 ), 
-         panel.border = element_rect( size = 0.25 ) ) + 
-  coord_flip()
-
-p <- ggplot( overall.df[ which( overall.df$variable == "overall" & overall.df$efxn != "m19_all_DLPC" & overall.df$efxn != "r15_all_DLPC" ), ], aes( x = residue, y = value, fill = category ) ) + 
-  geom_hline( yintercept = 0, color = "black", size = 0.25 ) + 
-  geom_bar( stat = "identity", color = "black", size = 0.15) + 
-  scale_fill_brewer( palette = "Pastel1" ) + 
-  #scale_fill_manual( values = c( "#984ea3", "#e41a1c", "#ffff33", "#377eb8", "#ff7f00") ) + 
-  scale_y_continuous( limits = c(-5, 1), breaks = c(-5, -4, -3, -2, -1, 0, 1), "Divergence" ) + 
-  scale_x_discrete( "" ) + 
-  facet_wrap( ~ efxn, scales = "free" ) + 
-  theme_bw() + 
-  theme( strip.background = element_blank(), 
-         legend.position = "none", 
-         text = element_text( size = 8 ), 
-         axis.text = element_text( size = 7 ), 
-         axis.line.x = element_line( size = 0.25 ), 
-         axis.line.y = element_line( size = 0.25 ), 
-         axis.ticks = element_line( size = 0.25 ), 
-         panel.grid.major = element_line( size = 0.15 ), 
-         panel.grid.minor = element_line( size = 0.15 ), 
-         panel.border = element_rect( size = 0.25 ) )
-print(p)
-
-q <- ggplot( overall.df[ which( overall.df$variable == "lipid_facing" & overall.df$efxn != "m19_all_DLPC" ), ], aes( x = residue, y = value, fill = category ) ) + 
-  geom_hline( yintercept = 0, color = "black" ) + 
-  geom_bar( stat = "identity", color = "black", size = 0.15) + 
-  scale_fill_manual( values = c( "#984ea3", "#e41a1c", "#ffff33", "#377eb8", "#ff7f00") ) + 
-  scale_y_continuous( limits = c(-16, 5), "Divergence" ) + 
-  scale_x_discrete( "" ) + 
-  facet_wrap( ~ efxn, scales = "free" ) + 
-  theme_bw() + 
-  theme( strip.background = element_blank(), 
-         legend.position = "none", 
-         text = element_text( size = 8 ), 
-         axis.text = element_text( size = 7 ), 
-         axis.line.x = element_line( size = 0.25 ), 
-         axis.line.y = element_line( size = 0.25 ), 
-         axis.ticks = element_line( size = 0.25 ), 
-         panel.grid.major = element_line( size = 0.25 ), 
-         panel.grid.minor = element_line( size = 0.25 ) )
-print(q)
-
-# Plot the native distribution
-native.pie <- ggplot( df2, aes( x = "", y = native, fill = Category ) ) + 
-  geom_bar( stat = "identity" ) + 
-  scale_fill_brewer( palette = "Pastel1" ) + 
-  coord_polar("y", start = 0 ) + 
-  theme( legend.position = "none", 
+#' Make a pie chart to represent tha distribution of amino acids in a set
+#' of proteins, divided by amino acid chemical class
+#'
+#' @param df Dataframe with amino acid composition data
+#'
+#' @return Plot object
+#'
+#' @examples
+#' make.aa.distribution.pie(df)
+#'
+make.aa.distribution.pie <- function(df) {
+  p <- ggplot( df, aes( x = "", y = native, fill = Category ) ) + 
+    geom_bar( stat = "identity" ) + 
+    scale_fill_brewer( palette = "Pastel1" ) + 
+    coord_polar("y", start = 0 ) + 
+    theme( legend.position = "none", 
          axis.line = element_blank(), 
          axis.text = element_blank(),
          axis.title = element_blank(), 
          axis.ticks = element_blank()  )
-
-m07.pie <- ggplot( df2, aes( x = "", y = m07, fill = Category ) ) + 
-  geom_bar( stat = "identity") + 
-  scale_fill_brewer( palette = "Pastel1" ) + 
-  coord_polar("y", start = 0 ) + 
-  theme( legend.position = "none", 
-         axis.line = element_blank(), 
-         axis.text = element_blank(),
-         axis.title = element_blank(), 
-         axis.ticks = element_blank()  )
-
-m12.pie <- ggplot( df2, aes( x = "", y = m12, fill = Category ) ) + 
-  geom_bar( stat = "identity" ) + 
-  scale_fill_brewer( palette = "Pastel1" ) + 
-  coord_polar("y", start = 0 ) + 
-  theme( legend.position = "none", 
-         axis.line = element_blank(), 
-         axis.text = element_blank(),
-         axis.title = element_blank(), 
-         axis.ticks = element_blank()  )
-
-m19.pie <- ggplot( df2, aes( x = "", y = m19, fill = Category ) ) + 
-  geom_bar( stat = "identity" ) + 
-  scale_fill_brewer( palette = "Pastel1" ) + 
-  coord_polar("y", start = 0 ) + 
-  theme( legend.position = "none", 
-         axis.line = element_blank(), 
-         axis.text = element_blank(),
-         axis.title = element_blank(), 
-         axis.ticks = element_blank() )
-
+  return(p)
+} 
